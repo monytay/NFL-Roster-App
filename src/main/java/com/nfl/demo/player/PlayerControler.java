f72d5f51a92b9124d1eeac769f924c4cd766dc1d
montay <t.majstorovic@outlook.com>
2026-01-21 11:16:55 +0100

    Roster By team finished

--- src/main/java/com/nfl/demo/player/PlayerControler.java
+++ src/main/java/com/nfl/demo/player/PlayerControler.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping(path = {"api/v1/player"})
 public class PlayerControler {
